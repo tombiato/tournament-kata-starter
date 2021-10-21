f4ae7be2fbf59d6bdd1a25514ac335fb0e0367f4
Tom Biato <tom.biato@ynov.com>
2021-10-21 20:45:39 +0200

    Init commit

---
+++ ts-nest/apps/tournament/src/app/repositories/phase-repository.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { TournamentPhase, Tournament } from '../api-model';
+import { TournamentRepositoryService } from './tournament-repository.service';
+
+@Injectable()
+export class TournamentPhaseRepositoryService {
+  private tournamentPhases = new Map<string, TournamentPhase>();
+  public addTournamentPhase(
+    phase: TournamentPhase,
+    tournamentId: string
+  ): void {
+    const tournamentRepository = new TournamentRepositoryService();
+    let tournamentToModify = tournamentRepository.getTournament(tournamentId);
+    tournamentToModify.phases;
+  }
+}
